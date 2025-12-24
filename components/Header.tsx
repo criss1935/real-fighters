1261dfa71ae76d802fb7aeb3c4b9675f124e8a0d
Cristopher <criss1935@users.noreply.github.com>
2025-12-24 03:00:43 -0600

    nuevo intento

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,6 +18,9 @@ export default function Header() {
   <Link href="/" className="hover:text-red-500 transition">
     Inicio
   </Link>
+  <Link href="/clases" className="hover:text-red-500 transition">
+              Clases
+                </Link>
   <Link href="/fighters" className="hover:text-red-500 transition">
     Peleadores
   </Link>
@@ -30,17 +33,14 @@ export default function Header() {
   <Link href="/admin" className="hover:text-red-500 transition">
     Admin
   </Link>
-  <Link href="/clases" className="hover:text-red-500 transition">
-              Clases
-              </Link>
-</nav>
-          <button className="md:hidden text-white">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
+  </nav>
+            <button className="md:hidden text-white">
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
     </header>
   );
 }
